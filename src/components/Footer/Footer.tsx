8362874d1dacc99d580b61346a7e35256dad3c48
karma-works <haegele@karma-works>
2026-06-13 06:37:17 +0200

    Fix X profile link typo

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -256,7 +256,7 @@ export const Footer = memo(function Footer() {
               </svg>
             </a>
             <a
-              href="https://x.com/moatshfit"
+              href="https://x.com/moatshift"
               target="_blank"
               rel="noopener noreferrer"
               className={styles.socialLink}
